76cebe2be92f8b4929509ff214c57da28859eb62
fengjinao <fengjinao@foxmail.com>
2025-09-28 16:47:10 +0800

    123

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/fengjinao.github.io/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
