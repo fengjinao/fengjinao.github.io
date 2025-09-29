913ebaf378f72b71153197cf025f226fa3eaa037
fengjinao <fengjinao@foxmail.com>
2025-09-29 09:27:09 +0800

    fix: deploy to fja branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/fengjinao.github.io/' : '',
+  base: '/', // 用户页面使用根路径
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
